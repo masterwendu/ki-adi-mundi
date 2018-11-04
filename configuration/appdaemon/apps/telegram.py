45cb1857f4759ec675a71b9c32c14b5fe153711f
Wendelin Peleska <wendu@pm.me>
2018-11-04 18:51:20 +0100

    fixed telegram appdaemon message

--- configuration/appdaemon/apps/telegram.py
+++ configuration/appdaemon/apps/telegram.py
@@ -1,30 +1,31 @@
 import appdaemon.plugins.hass.hassapi as hass
-import time
+import datetime
 
 class TelegramNotificationManager(hass.Hass):
 
   def initialize(self):
-    time = datetime.time(7, 00, 0)
+    time = datetime.time(7, 0, 0)
     self.run_daily(self.sendDailyNotification, time)
+    self.listen_state(self.sendDailyNotification, "binary_sensor.switch_158d0002506b00", new = "on")
 
     self.listen_event(self.sendDailyNotification, "telegram_command", command = "/status")
     self.listen_event(self.sendDailyNotification, "telegram_callback", command = "/status")
 
-  def sendDailyNotification(self, kwargs):
-      bedroomTemperature = self.get_state("sensor.temp_humidity_bedroom_temperature")
-      livingRoomTemperature = self.get_state("sensor.temp_humidity_livingroom_temperature")
+  def sendDailyNotification(self, *kwargs):
+      bedroomTemperature = float(self.get_state("sensor.temp_humidity_bedroom_temperature"))
+      livingRoomTemperature = float(self.get_state("sensor.temp_humidity_livingroom_temperature"))
       temperatureInsideAvg = (bedroomTemperature + livingRoomTemperature) / 2
-      wienTemperature = self.get_state("weather.temperatur_wien", "temperature")
+      wienTemperature = float(self.get_state("weather.temperatur_wien", attribute = "temperature"))
 
-      bedroomHumidity = self.get_state("sensor.temp_humidity_bedroom_humidity")
-      livingRoomHumidity = self.get_state("sensor.temp_humidity_livingroom_humidity")
+      bedroomHumidity = float(self.get_state("sensor.temp_humidity_bedroom_humidity"))
+      livingRoomHumidity = float(self.get_state("sensor.temp_humidity_livingroom_humidity"))
       humidityInsideAvg = (bedroomHumidity + livingRoomHumidity) / 2
-      wienHumidity = self.get_state("weather.temperatur_wien", "humidity")
+      wienHumidity = float(self.get_state("weather.temperatur_wien", attribute = "humidity"))
 
-      plantMoisture = self.get_state("sensor.flora_big_livingroom_moisture")
+      plantMoisture = float(self.get_state("sensor.flora_big_livingroom_moisture"))
 
       self.call_service(
           "notify/CARO_WENDI_GROUP",
-          title = "*Guten Morgen Caro und Wendi!*",
-          message = "Temperatur (Innen / Außen): *{} / {} °C*, Luftfeuchtigkeit (Innen / Außen): *{} / {} %*, Feuchtigkeit der Pflanze: *{} %*".format(temperatureInsideAvg, wienTemperature, humidityInsideAvg, wienHumidity, plantMoisture)
+          title = "*Guten Morgen!*",
+          message = "\n\nTemperatur:\n\t*🏠{:10.2f}°C\n\t⛅{:10.2f}°C*\n\nLuftfeuchtigkeit:\n\t*🏠{:10.2f}%\n\t⛅{:10.2f}%*\n\nFeuchtigkeit 🌿:\n\t*{:10.2f}%*".format(temperatureInsideAvg, wienTemperature, humidityInsideAvg, wienHumidity, plantMoisture)
           )
